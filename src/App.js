f6339f1e48c37b96608d65ea7ef69c60838a8e52
Anna Thomas <thomasanna85@gmail.com>
2020-11-24 14:04:33 +0900

    Game works
    Function for shuffling components OK, event listener to set clicked property/properies OK, score and best score OK.

--- src/App.js
+++ src/App.js
@@ -5,18 +5,46 @@ import Card from './Card'
 
 function App() {
   const [allFishies, setFishies] = useState(fishies);
-  const [scores, setScores] = useState({score: 0, bestScore: 0});
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
 
-  const cards = allFishies.map((fish, index) => <Card handleClick={handleClick} id={fish.id} key={fish.id} data={fish}/>);
+  //Make array of Card components first, then shuffles.
+  function shuffleCards(array){
+      const cards = array.map((fish, index) => <Card handleClick={handleClick} index={index} key={fish.id} data={fish}/>);
+      cards.sort(() => Math.random() - 0.5);
+      return cards;
+  }
 
   function handleClick(event){
-    console.log(event.currentTarget.dataset.id);
+//Get index of fishie, copy state array
+    const i = event.currentTarget.dataset.index;
+    const newArr = [...allFishies];
+ //If not clicked before, new array with click to "true" for that fishie, add 1 to score
+    if(newArr[i].clicked === false){
+        newArr[i].clicked = true
+        setScore(score + 1);
+        setFishies(newArr);
+      } 
+//If clicked before, new array with all clicked properties reset. Set best score
+//if applicable, reset current score.
+      else {
+        const newestArray = newArr.map(fishie => {
+          fishie.clicked = false;
+          return fishie;
+        });
+        setFishies(newestArray);
+        if(score > bestScore){
+          setBestScore(score);
+        }
+        setScore(0);
+      }
   }  
 
+//Shuffles cards for each render
   return (
     <div className="App">
-      <Scoreboard score={scores.score} bestScore={scores.bestScore}/>
-      {cards}
+      <Scoreboard score={score} bestScore={bestScore}/>
+      {shuffleCards(allFishies)}
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Card(props){
-    return(<div onClick={props.handleClick} data-id={props.id}>
+    return(<div onClick={props.handleClick} data-index={props.index}>
         <h1>{props.data.name}</h1>
         <img src={props.data.image} alt={props.data.name}/>
     </div>);
